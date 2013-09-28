f721dcedd916ba17c9071de9de4fd76aac3af8a9
Maki Tolentino <maki.tolentino.03@gmail.com>
2013-09-28 21:38:43 +0800

    - Added loading of MPSA from json. - Initial Design with logo.

--- app/js/app.js
+++ app/js/app.js
@@ -8,12 +8,5 @@ angular.module('mgb-inventory', ['mgb-inventory.controllers', 'mgb-inventory.fil
 				templateUrl: 'partials/partial1.html',
 				controller: 'MyCtrl1'
 			});
-			$routeProvider.when('/view2', {
-				templateUrl: 'partials/partial2.html',
-				controller: 'MyCtrl2'
-			});
-			$routeProvider.otherwise({
-				redirectTo: '/view1'
-			});
 		}
 	]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,58 +1,14 @@
-
 /* Controllers */
 angular.module('mgb-inventory.controllers', ['ngGrid'])
-	.controller('MgbInventoryCtrl', ['$scope',
-		function($scope) {
+	.controller('MgbInventoryCtrl', ['$scope','$http',
+		function($scope, $http) {
+			$http.get('mpsa/mpsa.json').success(function(data) {
+				$scope.myData = data;
+			});
+
 			$scope.filterOptions = {
 				filterText: ''
 			}
-
-			$scope.myData = [{
-				age: 400,
-				id: "001-90-CAR",
-				region: "ÑAR",
-				contractor: "Lepanto Consolidated Mining  Co. and Far Southeast Gold Resources Inc.",
-				dateApproved: "March 19, 1990",
-				commodity: "Gold and Copper",
-				area: "948.9695",
-				location: "Mankayan, Benguet",
-				headOffice: "21st Flr., BA-Lepanto Bldg. 8747 Paseo de Roxas, Makati City",
-				contactPerson: "Mr. Bryan U. Yap",
-				position: "President",
-				contactNumber: " 815-9447"
-			}, {
-				age: 399,
-				id: "002-90-XIII",
-				region: "XIII",
-				contractor: "HY Chromite Mining Development  Corp.",
-				dateApproved: "January 22, 1991",
-				commodity: "Chromite",
-				area: "972.0000",
-				metallicOrNonMetallic: "Metallic",
-				stage: "Partial Commercial Operation",
-				area: " 972.0000 ",
-				metallicorNonMetallic: "Metallic",
-				location: "Loreto (Dinagat Island), Surigao del Norte (Within Parcel III of Surigao Mineral Reservation)",
-				headOffice: "No. 6 G. Araneta Ave., QC",
-				contactPerson: "Mr. Lyonel Ty Tiao Hui",
-				position: "Director",
-				contactNumber: "715-12-31 715-10-35 0920-9214-033"
-			}, {
-				"age": 49,
-				"id": "345-2010-IVA",
-				"region": "IVA",
-				"contractor": "Rapid City Realty and Development Corp.",
-				"dateApproved": "June 9, 2010",
-				"commodity": "Silica and other associated mineral deposits",
-				"stage": "Exploration",
-				"area": "1,015.1690",
-				"metallicorNonMetallic": "Non-Metallic",
-				"location": "Teresa, Morong, Binangonan and Angono, Rizal",
-				"headOffice": "No. 167 Sumulong Highway , Mayamot, Antipolo, Rizal",
-				"contactPerson": "Veronica Iñiguez Lee",
-				"position": "President",
-				"contactNumber": ""
-			}];
 
 			$scope.gridOptions = {
 				data: 'myData',
@@ -60,6 +16,7 @@ angular.module('mgb-inventory.controllers', ['ngGrid'])
 				showFilter: true,
 				showFooter: true,
 				filterOptions: $scope.filterOptions,
+				jqueryUITheme: false,
 				showColumnMenu: true,
 				columnDefs: [{
 					field: 'id',
@@ -98,7 +55,8 @@ angular.module('mgb-inventory.controllers', ['ngGrid'])
 						if (prefix == 'Region:' || prefix == 'Metallic or Non-Metallic:') {
 							filter += prefix + '^' + value + '$;';
 						} else {
-							filterText += prefix + value + ';';
+							console.log(prefix + value + ';');
+							filter += prefix + value + ';';
 						}
 					}
 					return filter;
@@ -113,9 +71,6 @@ angular.module('mgb-inventory.controllers', ['ngGrid'])
 				filterText = addToFilterIfNotUndefinedAndNotEmpty(filterText, $scope.filterLocation, 'Location:');
 				filterText = addToFilterIfNotUndefinedAndNotEmpty(filterText, $scope.filterContactPerson, 'Contact Person:');
 
-				if (filterText !== '') {
-					filterText = filterText.substring(0, filterText.length - 1)
-				}
 				console.log(filterText);
 
 				$scope.filterOptions.filterText = filterText;
